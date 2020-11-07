ad947fef9cbdc36ca218876f1e5c1b7bb274b79a
w33zel <fu.please@gmail.com>
2020-11-07 22:51:30 +0100

    Initial Commit

---
+++ Barcody/BarcodyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodyApp.swift
+//  Barcody
+//
+//  Created by Christian Arlt on 07.11.20.
+//
+
+import SwiftUI
+
+@main
+struct BarcodyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
